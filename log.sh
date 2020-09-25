48b9503d4f0aea41d5c0c0afb35d161413e5741d
MaksimGolish <maksim.golish2002@gmail.com>
2020-09-25 13:19:35 +0300

    log fixed

--- log.sh
+++ log.sh
@@ -1,41 +1,14 @@
 #!/usr/bin/env bash
 
-warn_out() {
-  local BLUE="\033[1;34m"
-  local NC="\033[0m"
-  echo -e $(replace 'Warning:' "${BLUE}Warning: " "${BLUE}$@${NC}")
-}
-
-info_out() {
-  local YELLOW='\033[1;35m'
-  local NC='\033[0m'
-  echo -e $(replace 'Warning:' "${YELLOW}Warning: " "${YELLOW}$@${NC}")
-}
-
-foreach() {
-  while read value;
-  do
-	   eval "$* '$value'"
-  done
-}
-
-replace() {
-  local string="$3"
-    echo "${string//$1/$2}"
-}
-
-grep_warn() {
-  grep "(WW)" $log_file | foreach replace '\(WW\)' "Warning: "
-}
-
-grep_info() {
-  grep "(II)" $log_file | foreach replace '\(II\)' "Info: "
-}
-
 log () {
   log_file="log/X.log"
   ! file_exists $log_file && error "File $log_file not found"
   ! is_readable $log_file && error "file $log_file is not readable"
-  grep_warn | foreach warn_out
-  grep_info | foreach info_out
+  local warn=$(cat $log_file | awk '/\[/ && /WW/') &> /dev/null
+  local inf=$(cat $log_file | awk '/\[/ && /II/') &> /dev/null
+  local BLUE="\033[1;34m"
+  local MAGENTA='\033[1;35m'
+  local NC="\033[0m"
+  echo -e "${warn//"(WW)"/"${BLUE}Warn:${NC}"}"
+  echo -e "${inf//"(II)"/"${MAGENTA}Info:${NC}"}"
 }
